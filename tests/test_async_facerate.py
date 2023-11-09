6d9c0004fbe02360c4ec7c313781ee2133adc2a6
Danila Vershinin <ciapnz@gmail.com>
2023-11-10 02:27:58 +0800

    hh

--- tests/test_async_facerate.py
+++ tests/test_async_facerate.py
@@ -4,40 +4,6 @@ import pytest
 from facerate.async_rate import AsyncFaceRate
 from httpx import AsyncClient
 
-
-# This decorator is required to use async features with pytest
-@pytest.mark.asyncio
-async def test_get_score():
-    # Instantiate the AsyncFaceRate class
-    face_rate = AsyncFaceRate()
-
-    # Mocking AsyncClient to avoid actual HTTP requests in tests
-    with pytest.MonkeyPatch.context() as mp:
-        # Mock the post method
-        async def mock_post(*args, **kwargs):
-            class MockResponse:
-                def __init__(self):
-                    self.status_code = 200
-                    self.is_success = True
-                    # Mock the text response that you would get from face-score.com
-                    self.text = "<div class='facescore-text10'>82.5</div><div class='facescore-text12'>TOP 20%</div>"
-
-            return MockResponse()
-
-        # Apply the monkeypatch for httpx.AsyncClient.post to mock_post
-        mp.setattr(AsyncClient, "post", mock_post)
-
-        # Call the async get_score method
-        result = await face_rate.get_score('path/to/your/test/image.jpg')
-
-        # Parse the result as JSON
-        result_json = json.loads(result)
-
-        # Assert the values (these should match your mocked response)
-        assert result_json['score'] == 82.5
-        assert result_json['top'] == 20.0
-
-
 # List of tuples containing the filename and the expected score range as (min, max)
 test_images = [
     pytest.param("p1.jpg", (50, 60), id='p1'),
